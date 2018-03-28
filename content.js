0968a26747649b591f91025ed550aa85e280b5fb
Chris <mrchrisrobles@gmail.com>
2018-03-28 11:48:30 -0700

    fix for erroneous time format

--- content.js
+++ content.js
@@ -5,7 +5,21 @@ vid.addEventListener("ended", function() {
 	var now = new Date();
 	var min = now.getMinutes();
 	var hour = now.getHours();
-	alert("Time: " + hour + ":" + min);
+	if (hour > 12) {
+		hour -= 12;
+		if (min < 10) {
+			alert("Time: " + hour + ":0" + min);
+		} else {
+			alert("Time: " + hour + ":" + min);
+		}
+	} else {
+		if (min < 10) {
+			alert("Time: " + hour + ":0" + min);
+		} else {
+			alert("Time: " + hour + ":" + min);
+		}
+	}
+	
 
 	//$("body").append('<dialog>This is a dialog.<br><button>Close</button></dialog>');
 	//$( "#dialog" ).dialog("open");
